e10c6f652174b2a6e88f1ec835f5c09606e6d290
SyahrulGigaWahyudi <syahrulgiga@gmail.com>
2025-03-03 08:49:04 +0700

    Add files via upload

---
+++ pertemuan-2/variable_user.php
@@ -0,0 +1,10 @@
+<?php
+    // mendefisinikan variable
+    $nama = "sina";
+    $umur = 15;
+    $bb =    40;
+
+    echo "nama saya adalah: " . $nama;
+    echo "<br/>umur: " .$umur."tahun";
+    echo "<br/>berat badan: " .$bb."kg";
+?>
